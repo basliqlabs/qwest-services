db56ea89aa42ff998c97c11d9a3cc9e1cff0bb5c
atareversei (vivobook) <atareversei@gmail.com>
2025-02-03 19:04:34 +0330

    refactor: extract repetitive delivery handler code into echoutil package

--- delivery/httpserver/userhandler/login.go
+++ delivery/httpserver/userhandler/login.go
@@ -4,17 +4,16 @@ import (
 	"net/http"
 
 	"github.com/basliqlabs/qwest-services/dto/userdto"
-	"github.com/basliqlabs/qwest-services/pkg/contextutil"
+	"github.com/basliqlabs/qwest-services/pkg/echoutil"
 	"github.com/basliqlabs/qwest-services/pkg/envelope"
-	"github.com/basliqlabs/qwest-services/pkg/translation"
 	"github.com/labstack/echo/v4"
 )
 
 // userLogin lets a user get an authorization token
 //
 //	@Summary		User login
-//	@Description	Authenticate a user with username and password
-//	@Tags			users
+//	@Description	Authenticate a user with username/phone/email and password
+//	@Tags			  users
 //	@Accept			json
 //	@Produce		json
 //	@Param			request	body		userdto.LoginRequest	true	"Login credentials"
@@ -24,32 +23,18 @@ import (
 //	@Router			/users/login [post]
 func (h Handler) userLogin(c echo.Context) error {
 	ctx := c.Request().Context()
-	lang := contextutil.GetLanguage(ctx)
 	req := new(userdto.LoginRequest)
-
 	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, envelope.New(false).WithError(&envelope.ResponseError{
-			Code:    envelope.ErrBadRequest,
-			Message: translation.T(lang, "bad_request", nil),
-		}))
+		return echoutil.HandleBadRequest(c)
 	}
-
 	validationErrors, err := h.validator.Login(ctx, req)
-
 	if err != nil {
-		return c.JSON(http.StatusUnprocessableEntity, envelope.New(false).WithError(&envelope.ResponseError{
-			Code:    envelope.ErrInvalidInput,
-			Message: translation.T(lang, "invalid_input", nil),
-			Fields:  validationErrors,
-		}))
+		echoutil.HandleUnprocessableContent(c, validationErrors)
 	}
-
 	res, err := h.service.Login(ctx, req)
-
 	if err != nil {
-		return c.JSON(http.StatusUnauthorized, envelope.FromRichError(err))
+		echoutil.HandleInternalServerError(c, err)
 	}
-
 	return c.JSON(http.StatusOK, envelope.New(true).WithData(map[string]any{
 		"login": res,
 	}))
